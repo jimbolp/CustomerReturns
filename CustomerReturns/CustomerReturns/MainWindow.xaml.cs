c0effd4f3e75ccb786446f5a34ec35ba4a0866e9
Yavor Georgiev <jimbolp@gmail.com>
2017-12-01 17:29:25 +0200

    Done! The textboxes are not used but are left just in case

--- CustomerReturns/CustomerReturns/MainWindow.xaml.cs
+++ CustomerReturns/CustomerReturns/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,6 +33,7 @@ namespace CustomerReturns
         public List<long> NotFound { get; set; } = new List<long>();
         public string Subject { get; set; } = "";
         public string Body { get; set; } = "";
+        public bool FormatReady { get; set; } = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -52,9 +54,123 @@ namespace CustomerReturns
 
         private void FreeTextTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            
+        }
+        public IEnumerable<TextRange> GetAllWordRanges(FlowDocument document, string invoice)
+        {
+            string pattern = invoice;
+            TextPointer pointer = document.ContentStart;
+            while (pointer != null)
+            {
+                if (pointer.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    string textRun = pointer.GetTextInRun(LogicalDirection.Forward);
+                    MatchCollection matches = Regex.Matches(textRun, pattern);
+                    foreach (Match match in matches)
+                    {
+                        int startIndex = match.Index;
+                        int length = match.Length;
+                        TextPointer start = pointer.GetPositionAtOffset(startIndex);
+                        TextPointer end = start.GetPositionAtOffset(length);
+                        yield return  new TextRange(start, end);
+                    }
+                }
+                pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
+            }
+        }
+        private void FormatBeforeSend()
+        {
+            if (invoices.Count != 0)
+            {
+                string multi = invoices.Count != 1 ? "s" : "";
+                Subject = $"Customer Returns - load old invoice{multi} into invoice history";
+
+                Body = $"Please load the following invoice{multi}:" + Environment.NewLine;
+                foreach (InvoiceToSend invoice in invoices)
+                {
+                    Body += Environment.NewLine
+                        + invoice;
+                }
+                if (invoices.Count != 1)
+                {
+                    Body += Environment.NewLine
+                        + Environment.NewLine
+                        + $"The invoices are not available in the invoice history (DKHIS) and have to be reloaded!";
+                }
+                else
+                {
+                    Body += Environment.NewLine
+                        + Environment.NewLine
+                        + $"The invoice is not available in the invoice history (DKHIS) and has to be reloaded!";
+                }
+            }
+            InfoLabel.Dispatcher.Invoke(() =>
+            {
+                InfoLabel.Text = "Subject: "
+                  + Environment.NewLine
+                  + Subject
+                  + Environment.NewLine
+                  + Environment.NewLine
+                  + "Body: "
+                  + Environment.NewLine
+                  + Body;
+
+                if (NotFound.Count != 0)
+                {
+                    InfoLabel.Text += Environment.NewLine + "Следните фактури не бяха намерени в базата: " + Environment.NewLine;
+                    foreach (long invoice in NotFound)
+                    {
+                        InfoLabel.Text += invoice + ", ";
+                    }
+                    if(InfoLabel.Text.LastIndexOf(',') >= 0)
+                        InfoLabel.Text.Remove(InfoLabel.Text.LastIndexOf(','), 1);
+                }
+            });
+        }
+
+        private void Reset_Click(object sender, RoutedEventArgs e)
+        {
+            ResetFields();
+            FreeTextTextBox.Document = new FlowDocument(); 
+        }
+
+        private void ResetFields()
+        {
+            invoices = new List<InvoiceToSend>();
+            NotFound = new List<long>();
+            InfoLabel.Text = "";
+            //FreeTextTextBox.Document = new FlowDocument();
+            InvoiceNumberTextBox.Text = "";
+            CustomerTextBox.Text = "";
+            DateTextBox.Text = "";
+            BranchTextBox.Text = "";
+            FormatReady = false;
+        }
+        private void GenerateMail_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FormatReady)
+                {
+                    SendEmail.Send(Subject, Body);
+                }
+                else
+                {
+                    MessageBox.Show("Не е генериран имейл за изпращане.", "Внимание!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GenerateProform_Click(object sender, RoutedEventArgs e)
+        {
+            ResetFields();
             Thread t = new Thread(() =>
             {
-                List<long> invoiceNumbers = Helper.SplitText(FreeTextTextBox.Dispatcher.Invoke(() => FreeTextTextBox.Text));
+                List<long> invoiceNumbers = Helper.SplitText(FreeTextTextBox.Dispatcher.Invoke(() => new TextRange(FreeTextTextBox.Document.ContentStart, FreeTextTextBox.Document.ContentEnd).Text));
 
                 if (invoiceNumbers == null)
                     return;
@@ -88,10 +204,16 @@ namespace CustomerReturns
                         }
                         if (sale == null)
                         {
-                            NotFound.Add(invoice);
+                            if(!NotFound.Any(i => i == invoice))
+                                NotFound.Add(invoice);
                         }
                         else
                         {
+                            if (invoices.Count != 0)
+                            {
+                                if (invoices.Any(i => i.InvoiceNumber == sale.DocNo))
+                                    continue;
+                            }
                             invoices.Add(new InvoiceToSend()
                             {
                                 CustomerNumber = Helper.GetControlDigit(sale.CustomerID ?? 0),
@@ -99,47 +221,24 @@ namespace CustomerReturns
                                 InvoiceDate = sale.DocDate,
                                 Branch = (int)Helper.GetBranch(sale.DocNo.ToString())
                             });
+                            FreeTextTextBox.Dispatcher.Invoke(() =>
+                            {
+                                IEnumerable<TextRange> wordRanges = GetAllWordRanges(FreeTextTextBox.Document, sale.DocNo.ToString());
+                                foreach (TextRange wordRange in wordRanges)
+                                {
+                                    if (wordRange.Text == sale.DocNo.ToString())
+                                    {
+                                        wordRange.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.LightSeaGreen);
+                                    }
+                                }
+                            });
                         }
                     }
                 }
-                if (invoices.Count != 0)
-                    CreateEmail();
+                FormatBeforeSend();
+                FormatReady = true;
             });
             t.Start();
-        }
-
-        private void CreateEmail()
-        {
-            string multi = invoices.Count != 1 ? "s" : "";
-            Subject = $"Customer Returns - load old invoice{multi} into invoice history";
-
-            Body = $"Please load the following invoice{multi}:" + Environment.NewLine;
-            foreach(InvoiceToSend invoice in invoices)
-            {
-                Body += Environment.NewLine 
-                    + invoice;
-            }
-            if (invoices.Count != 1)
-            {
-                Body += Environment.NewLine 
-                    + Environment.NewLine 
-                    + $"The invoices are not available in the invoice history (DKHIS) and have to be reloaded!";
-            }
-            else
-            {
-                Body += Environment.NewLine 
-                    + Environment.NewLine 
-                    + $"The invoice is not available in the invoice history (DKHIS) and has to be reloaded!";
-            }
-
-            InfoLabel.Dispatcher.Invoke(() => InfoLabel.Text = "Subject: "
-                + Environment.NewLine
-                + Subject
-                + Environment.NewLine
-                + Environment.NewLine
-                + "Body: "
-                + Environment.NewLine
-                + Body);
         }
     }
 }
